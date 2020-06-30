b9d01f55a5c13a91999b9d0f76ad3ee00906d42d
sadhav <sadhav@legalzoom.com>
2020-06-30 23:07:47 +0530

    react native app globo

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import { Home } from './app/views/Home.js';
+import { Contact } from './app/views/Contact.js';
+import { StackNavigator } from 'react-navigation';
 
+import { Register } from './app/views/Register.js';
+import { Login } from './app/views/Login.js';
+
+import { Quiz } from './app/views/Quiz.js';
+import { Finish } from './app/views/QuizFinish.js';
+
+import { About } from './app/views/About.js';
+
+const MyRoutes = StackNavigator({
+  HomeRT: {
+    screen: Home
+  },
+  ContactRT: {
+    screen: Contact
+  },
+  RegisterRT: {
+    screen: Register
+  },
+  LoginRT: {
+    screen: Login
+  },
+  QuizRT: {
+    screen: Quiz
+  },
+  FinishRT: {	
+    screen: Finish
+  },
+  AboutRT: {	
+    screen: About
+ }
+},
+  {
+    initialRouteName: 'HomeRT'
+  }
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <Home/> 
+      <MyRoutes /> 
     );
   }
 }
